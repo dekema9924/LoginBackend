e976d3c6eec1bd0b1b48cbe8545aa4198c679223
Daniel <daniel@Daniels-MacBook-Pro.local>
2024-09-11 22:25:58 -0400

    cors config

--- server.js
+++ server.js
@@ -15,7 +15,12 @@ const cors = require('cors');
 //route middleware
 app.use('/routes', route);
 app.use(cookieParser())
-app.use(cors())
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 
 
@@ -23,7 +28,7 @@ app.get('/', (req,res)=>{
     res.redirect('/routes')
 })
 
-app.listen(port, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
     console.log(`server open on http://localhost:${port}`)
     mongoose.connect(process.env.MONGODBURL)
     .then(console.log('connected to db'))
